a896888b2e7fe1f143191db9a8d5f110affae205
Serene_Dream <3345973813@qq.com>
2024-08-06 11:31:29 +0800

    创建一个 Student 的实例对象

---
+++ src/main/java/day20240806/StudentTest.java
@@ -0,0 +1,12 @@
+package day20240806;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("name", 18);
+        s1.setName("qq");
+        s1.setAge(12);
+        System.out.println(s2.getName());
+        System.out.println(s2.getAge());
+    }
+}
