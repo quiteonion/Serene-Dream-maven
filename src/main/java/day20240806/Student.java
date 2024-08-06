1a2c1ed345a03b25537baf8f661aa2b6c3490b7b
Serene_Dream <3345973813@qq.com>
2024-08-06 11:35:32 +0800

    添加 @NoArgsConstructor 和 @AllArgsConstructor 后发现实现类可以使用 空参和全参构造

--- src/main/java/day20240806/Student.java
+++ src/main/java/day20240806/Student.java
@@ -1,10 +1,14 @@
 package day20240806;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Student {
     String name;
