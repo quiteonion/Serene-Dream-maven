ba7b4f54f56b772fda919cb79011f16b43e9676c
Serene_Dream <3345973813@qq.com>
2024-08-12 15:55:35 +0800

    MySQL - 当信息有来自于用户的，为了安全考虑全都使用 preparedStatement

--- src/test/java/day20240812/JDBCTest.java
+++ src/test/java/day20240812/JDBCTest.java
@@ -25,4 +25,25 @@ public class JDBCTest {
             System.out.println(id + "-" + name + "-" + balance);
         }
     }
+
+    @Test
+    public void testPreparedStatement() throws SQLException {
+        //获取连接
+        String url = "jdbc:mysql://localhost:3306/demo01";
+        String user = "root";
+        String password = "lcw0909.";
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String query = "select id, name, balance from user where id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1,1);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        //打印
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            int balance = resultSet.getInt("balance");
+            System.out.println(id + "-" + name + "-" + balance);
+        }
+    }
 }
