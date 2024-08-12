35788fbae1bc181feb141c59cdf4e906bcb39d1d
Serene_Dream <3345973813@qq.com>
2024-08-12 16:04:44 +0800

    MySQL - 创建 connection 的代码的整理

--- src/test/java/day20240812/JDBCTest.java
+++ src/test/java/day20240812/JDBCTest.java
@@ -1,17 +1,16 @@
 package day20240812;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 
 public class JDBCTest {
+    static Connection connection;
     @Test
     public void testStatement() throws SQLException {
-        //获取连接
-        String url = "jdbc:mysql://localhost:3306/demo01";
-        String user = "root";
-        String password = "lcw0909.";
-        Connection connection = DriverManager.getConnection(url, user, password);
+
+
         //定义对应的属性
         String query = "select id, name, balance from user";
         //将内部属性引出
@@ -29,15 +28,11 @@ public class JDBCTest {
 
     @Test
     public void testPreparedStatement() throws SQLException {
-        //获取连接
-        String url = "jdbc:mysql://localhost:3306/demo01";
-        String user = "root";
-        String password = "lcw0909.";
-        Connection connection = DriverManager.getConnection(url, user, password);
 
         String query = "select id, name, balance from user where id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setInt(1,1);
+
+        preparedStatement.setInt(1, 1);
         ResultSet resultSet = preparedStatement.executeQuery();
         //打印
         while (resultSet.next()) {
@@ -47,5 +42,14 @@ public class JDBCTest {
             System.out.println(id + "-" + name + "-" + balance);
         }
         connection.close();
+    }
+
+    @BeforeAll
+    private static void getConnection() throws SQLException {
+        //获取连接
+        String url = "jdbc:mysql://localhost:3306/demo01";
+        String user = "root";
+        String password = "lcw0909.";
+         connection = DriverManager.getConnection(url, user, password);
     }
 }
